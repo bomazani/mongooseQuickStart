33e266b97dd01742c475b22f73402ba69dd14f67
Bob Hunt <bob.hunt.email@gmail.com>
2018-07-27 12:12:57 -0400

    worked out some issues with Taylor

--- app.js
+++ app.js
@@ -5,36 +5,47 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     // we're connected!
+
+
+    const kittySchema = new mongoose.Schema({
+        name: String,
+        color: String
+    });
+
+    kittySchema.methods.speak = function() {
+        const greeting = this.name
+            ? "Meow name is " + this.name
+            : "I dont have a name";
+        console.log(greeting);
+    }
+
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const silence = new Kitten({ name: 'Silence', color: 'White', tail: true });
+    console.log(silence.name); // 'Silence'
+
+    
+
+
+    let fluffy = new Kitten({ name: 'fluffy' });
+    // fluffy.speak(); // "Meow name is fluffy"
+
+    fluffy.save(function (err, fluffy) {
+        if (err) return console.error(err);
+        fluffy.speak();
+    });
+
+    Kitten.find(function (err, kittens) {
+        if (err) return console.error(err);
+        console.log(kittens);
+    });
+
+    const catFind = function(err,cats){
+        if (err) return console.error(err);
+        console.log(cats)
+    };
+
+
+    Kitten.find({ name: /^fluff/ }, catFind);
+        
 });
-
-const kittySchema = new mongoose.Schema({
-    name: String
-});
-
-const Kitten = mongoose.model('Kitten', kittySchema);
-
-const silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
-
-kittySchema.methods.speak = function() {
-    const greeting = this.name
-        ? "Meow name is " + this.name
-        : "I dont have a name";
-    console.log(greeting);
-}
-
-
-let fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak(); // "Meow name is fluffy"
-
-fluffy.save(function (err, fluffy)) {
-    if (err) return console.error(err);
-    fluffy.speak();
-});
-
-Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-});
-
-Kitten.find({ name: /fluff/ }, callback);
